ca58a8978259ff4a6c33787eaf31597bc39ee25b
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-14 23:14:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require_relative 'spec helper'
+
+def my_collection(array)
+  i = 0 
+  my_collect_array =[]
+  
+  while i < array.length  
+  my_collect_array << yield array[i]
+  
+  i += 1 
+  end
+    return my_collect_array
+end
 
 
