7e8945aee75d99493fdfe3faff3c70b6db428ec6
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-14 23:41:53 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'rspec'
+
+def my_collect(array)
+  i = 0 
+  my_collect_array =[]
+  
+  while i < array.length  
+  my_collect_array << yield(array[i])
+  
+  i += 1 
+  end
+     my_collect_array
+end
 
 
